9387cecf3be7078ba4fa22286331a75600b06e7b
Kevin <kevinmarlis@gmail.com>
2022-03-21 12:07:33 -0700

    Updated output directory

--- SLI_pipeline/conf/global_settings.py
+++ SLI_pipeline/conf/global_settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 OUTPUT_DIR = Path('/Users/marlis/Developer/SLI/sealevel_output/')
-OUTPUT_DIR = Path('/Users/marlis/Developer/SLI/dev_output/')
 
 SOLR_HOST = 'http://localhost:8983/solr/'
 SOLR_COLLECTION = 'sli_dev'
